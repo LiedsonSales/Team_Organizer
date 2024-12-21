13aa8840385f00eb4c9eeb6e358ea7b70f9f2f40
Liedson Sales <liedson.r.sales@gmail.com>
2024-12-21 00:34:35 -0300

    missing adjustments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,30 +3,44 @@ const button = document.querySelector('.submit-button');
 const qtdEquipes = document.querySelector('.qtd-equipes');
 const qtdJogadores = document.querySelector('.qtd-jogadores');
 
-let jogadores;
 
-function randomLuck(lista) {
+function randomChoice(lista) {
     return Math.floor(Math.random() * lista.length);
 }
 
 
+function criaEquipes(equipes, times) {
+    for (let i = 1;i <= equipes; i++) {
+        times.push([]);
+    }
+    return times
+}
 
+let jogadores;
 button.addEventListener('click', function(event) {
     const numEquipes = Number(qtdEquipes.value);
     const numJogadores = Number(qtdJogadores.value);
 
     jogadores = players.value.split('\n');
 
-    console.log(jogadores.length, '-> tamanho');
-    let nomes;
+    const times = [];
+    criaEquipes(numEquipes, times);
+    console.log(times)
 
-    for (let i = 0; jogadores.length; i++) {
-        const randomic = randomLuck(jogadores);
-        console.log(jogadores);
+    let nomes;
+    let indice = 0;
+    for (let i = 0;i = jogadores.length; i++) {
+        const randomic = randomChoice(jogadores);
 
         nomes = jogadores.splice(randomic, 1).join();
-        
-        console.log(randomic, '-> índice');
-        console.log(nomes)
-    }
+
+        times[indice].push(nomes);
+        if (times[indice].length == numJogadores) {
+            indice++;
+        }
+    };
+
+    const valoresFiltrados = times.filter(valores => valores);
+
+    console.log(valoresFiltrados);
 })
