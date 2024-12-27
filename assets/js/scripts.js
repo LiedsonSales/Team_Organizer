8fd0e2c32d28d96c1a1164561c89ade392e0d4b2
Liedson Sales <liedson.r.sales@gmail.com>
2024-12-26 22:33:15 -0300

    teste dia 26/12

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ const players = document.querySelector("#player-names");
 const button = document.querySelector('.submit-button');
 const qtdEquipes = document.querySelector('.qtd-equipes');
 const qtdJogadores = document.querySelector('.qtd-jogadores');
+const body = document.querySelector('.body');
 
 
 function randomChoice(lista) {
@@ -13,34 +14,48 @@ function criaEquipes(equipes, times) {
     for (let i = 1;i <= equipes; i++) {
         times.push([]);
     }
-    return times
+    return times;
 }
 
-let jogadores;
+function criaSection() {
+    const section = document.createElement('section');
+    section.setAttribute('class', 'section-02');
+    body.appendChild(section);
+    return section;
+}
+
+
 button.addEventListener('click', function(event) {
+    criaSection();
+
     const numEquipes = Number(qtdEquipes.value);
     const numJogadores = Number(qtdJogadores.value);
 
-    jogadores = players.value.split('\n');
+    let jogadores = players.value.split('\n');
 
     const times = [];
     criaEquipes(numEquipes, times);
     console.log(times)
+    // try {
+        let nomes;
+        let indice = 0;
+        for (let i = 0;i = jogadores.length; i++) {
+            const randomic = randomChoice(jogadores);
 
-    let nomes;
-    let indice = 0;
-    for (let i = 0;i = jogadores.length; i++) {
-        const randomic = randomChoice(jogadores);
+            nomes = jogadores.splice(randomic, 1).join();
 
-        nomes = jogadores.splice(randomic, 1).join();
-
-        times[indice].push(nomes);
-        if (times[indice].length == numJogadores) {
-            indice++;
+            times[indice].push(nomes);
+            if (times[indice].length == numJogadores) {
+                indice++;
         }
     };
+    // } catch {
+    //     console.log(TypeError());
+    // };
+    
+    console.log(times);
 
     const valoresFiltrados = times.filter(valores => valores);
 
-    console.log(valoresFiltrados);
+
 })
